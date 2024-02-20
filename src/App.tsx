b362ae9068301193b54e7623eaba68bdad0a221c
daniel_haida <danilgajda22@gmail.com>
2024-02-20 10:03:31 +0200

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import React, {useEffect} from "react"
-import {useDispatch, useSelector} from "react-redux"
-import {fetchProducts} from "./store/Products/store"
-
 import "./App.css"
-import {AppDispatch} from "./store/store"
-import {selectProducts} from "./store/Products/selectors"
+import {RouterProvider} from "react-router-dom"
+import {router} from "./router/Router"
 
 function App() {
-	const dispatch = useDispatch<AppDispatch>()
-	const products = useSelector(selectProducts)
-
-	useEffect(() => {
-		dispatch(fetchProducts())
-	}, [dispatch])
 	return (
 		<div className="App">
-			{products &&
-				products.map((product) => (
-					<div className="product-card" key={product.id}>
-						<img src={product.images[0]} alt={product.title} />
-						<div className="product-details">
-							<h2>{product.title}</h2>
-							<p>{product.description}</p>
-							<p>Price: ${product.price}</p>
-						</div>
-					</div>
-				))}
+			<RouterProvider
+				router={router}
+				fallbackElement={<p>Loading...</p>}
+			/>
 		</div>
 	)
 }
