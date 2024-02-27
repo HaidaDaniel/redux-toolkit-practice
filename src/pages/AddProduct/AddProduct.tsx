08446a6cc998041ae16526b1c996d7e855ee3c9f
daniel_haida <danilgajda22@gmail.com>
2024-02-27 16:36:05 +0200

    add simple validation and inputs in form

--- src/pages/AddProduct/AddProduct.tsx
+++ src/pages/AddProduct/AddProduct.tsx
@@ -2,19 +2,36 @@ import {useEffect} from "react"
 import {useSelector} from "react-redux"
 import {fetchCategories} from "../../store/Categories/store"
 import {RootState, useAppDispatch} from "../../store/store"
+import {yupResolver} from "@hookform/resolvers/yup"
 import {useForm, SubmitHandler} from "react-hook-form"
+import * as yup from "yup"
 
-type Inputs = {
-	exampleRequired: string
-}
+// type Inputs = {
+// 	title: string
+// 	price: number
+// 	description: string
+// 	categoryId: number
+// 	images: string[]
+// }
+
+const schema = yup
+	.object({
+		title: yup.string().required(),
+		price: yup.number().required(),
+		description: yup.string().required(),
+		categoryId: yup.number().required(),
+		images: yup.array().of(yup.string().url()),
+	})
+	.required()
 
 function AddProduct() {
 	const {
 		register,
 		handleSubmit,
-		watch,
 		formState: {errors},
-	} = useForm<Inputs>()
+	} = useForm({
+		resolver: yupResolver(schema),
+	})
 	const dispatch = useAppDispatch()
 	const categories = useSelector(
 		(state: RootState) => state.categories.Categories
@@ -24,10 +41,18 @@ function AddProduct() {
 		dispatch(fetchCategories())
 	}, [dispatch])
 
-	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+	const onSubmit = (data: any) => console.log(data)
 
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
+			<input {...register("title")} />
+			<p>{errors.title?.message}</p>
+
+			<input type="number" {...register("price")} />
+			<p>{errors.price?.message}</p>
+
+			<textarea {...register("description")} />
+			<p>{errors.description?.message}</p>
 			<select name="category">
 				{categories &&
 					categories.map((category) => (
@@ -36,7 +61,9 @@ function AddProduct() {
 						</option>
 					))}
 			</select>
-			{errors.exampleRequired && <span>This field is required</span>}
+			<p>{errors.categoryId?.message}</p>
+			<input {...register("images")} />
+			<p>{errors.images?.message}</p>
 			<input type="submit" />
 		</form>
 	)
